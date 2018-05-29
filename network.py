2cd5bdec7ca5e2ee1536fdb8df2343c49f66babc
rybin <forknispoon@gmail.com>
2018-05-29 15:56:47 +0300

    comment network.py

--- network.py
+++ network.py
@@ -8,34 +8,84 @@ bias2 = np.load('./f/bias2.npy')
 
 
 def activation(put):
-    ''' Сигмоидальная функция активации '''
+    """ Сигмоидальная функция активации
+
+    Parametrs
+    ---------
+    put : int
+        Полученное нейроном значение
+
+    Returns
+    -------
+    int
+        Возвращаемое нейроном значение
+    """
     return 1 / (1 + np.exp(-put))
 
 
 def activationDer(put):
-    ''' Производная сигмоидальной функции активации '''
+    """ Производная сигмоидальной функции активации
+
+    Parametrs
+    ---------
+    put : int
+
+    Returns
+    -------
+    int
+    """
     return activation(put) * (1 - activation(put))
 
 
-# def activation(put):
-#     ''' Функция активации '''
-#     return 2 / (1 + np.exp(-put)) - 1
-
-
-# def activationDer(put):
-#     ''' Производная функции активации '''
-#     return (1 / 2) * (1 + activation(put)) * (1 - activation(put))
-
-
 def first(inp, act):
+    """ Скрытый слой нейронной сети
+
+    Parametrs
+    ---------
+    inp : numpy.ndarray
+        Сигнал поданый через связи между входным и скрытым слое
+    act : function
+        Функция активации
+
+    Yields
+    ------
+    int
+        Выходное значение одного нейрона скрытого слоя
+    """
     for weight, bias in zip(layer1, bias1):
         yield act(np.sum(np.multiply(weight, inp)) + bias)
 
 
 def second(first, act):
+    """ Выходной слой нейронной сети
+
+    Parametrs
+    ---------
+    first : numpy.ndarray
+        Сигнал поданый через связи между скрытым и выходным слое
+    act : function
+        Функция активации
+
+    Yields
+    ------
+    int
+        Выходное значение одного нейрона выходного слоя
+    """
     for weight, bias in zip(layer2, bias2):
         yield act(np.sum(np.multiply(weight, first)) + bias)
 
 
 def call(inp):
+    """ Вызов нейронное сети
+
+    Parametrs
+    ---------
+    inp : numpy.ndarray
+        Входные сигналы
+
+    Returns
+    -------
+    generator
+        Генератор возвращающий выходные значения нейронной сети
+    """
     return second(list(first(inp, activation)), activation)
