674f10a5bac912fed5deb5591ece1074c22f6a0d
rybin <forknispoon@gmail.com>
2018-06-13 00:03:28 +0300

    start convlearn

--- convlearn.py
+++ convlearn.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3.6
+import numpy as np
+import convnet
+
+alpha = 1
+
+
+def convlearn(test, error):
+    dX = np.zeros(test.shape)
+    dW = np.zeros(convnet.kernel.shape)
+    out = convnet.convlayer(test)
+    for i in range(error.shape[0]):
+        for j in range(error.shape[1]):
+            dX[i:i + dW[0], j:j + dW[1]] += convnet.kernel * error
+            dW += test[i:i + dW[0], j:j + dW[1]] * error
