ec5d9da4f7f59c97dba6bd59f9caff11bc387c89
rybin <forknispoon@gmail.com>
2018-06-13 22:46:55 +0300

    keras 2d input

--- keras2.py
+++ keras2.py
@@ -8,7 +8,8 @@ from keras.models import load_model
 model = load_model('mymodel')
 
 pic = sys.argv[1]
-inp = np.array((255 - cv2.imread(pic, 0)) / 255).reshape(1, 256)
+inp = np.array([(255 - cv2.imread(pic, 0)) / 255])
+# inp = np.array((255 - cv2.imread(pic, 0)) / 255).reshape(1, 256)
 # print(inp)
 
 res = model.predict(inp, batch_size=32)[0]
--- kerastest.py
+++ kerastest.py
@@ -5,14 +5,15 @@ import numpy as np
 from pathlib import Path
 import keras
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Flatten
 
 nums = 52
 
 inp = []
 char = []
 for pic in Path().glob(r'pic/[a-zA-Z]*.png'):
-    inp.append(np.array((255 - cv2.imread(str(pic), 0)) / 255).reshape(1, 256)[0])
+    # inp.append(np.array((255 - cv2.imread(str(pic), 0)) / 255).reshape(1, 256)[0])
+    inp.append((255 - cv2.imread(str(pic), 0)) / 255)
     char.append(string.ascii_letters.index(pic.stem[0]))
     # char.append(list(map(lambda x: x == pic.stem[0], string.ascii_letters)))
 
@@ -24,8 +25,8 @@ char = keras.utils.to_categorical(char, nums)
 
 model = Sequential()
 
-# model.add(Dense(units=64, activation='relu', input_dim=100))
-model.add(Dense(units=128, activation='relu', input_shape=(256,)))
+model.add(Dense(units=128, activation='relu', input_shape=(16, 16)))
+model.add(Flatten())
 model.add(Dense(units=64, activation='relu'))
 model.add(Dense(units=nums, activation='softmax'))
 
