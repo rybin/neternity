4acb6fac3901d0a66f57bb4f40895aa93235f9ca
rybin <forknispoon@gmail.com>
2018-06-13 21:34:28 +0300

    test keras

---
+++ keras2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.6
+import cv2
+import numpy as np
+import sys
+import string
+from keras.models import load_model
+
+model = load_model('mymodel')
+
+pic = sys.argv[1]
+inp = np.array((255 - cv2.imread(pic, 0)) / 255).reshape(1, 256)
+# print(inp)
+
+res = model.predict(inp, batch_size=32)[0]
+res = res.tolist()
+index = res.index(max(res))
+print(index)
+print(string.ascii_letters[index])
---
+++ kerastest.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3.6
+import string
+import cv2
+import numpy as np
+from pathlib import Path
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+nums = 52
+
+inp = []
+char = []
+for pic in Path().glob(r'pic/[a-zA-Z]*.png'):
+    inp.append(np.array((255 - cv2.imread(str(pic), 0)) / 255).reshape(1, 256)[0])
+    char.append(string.ascii_letters.index(pic.stem[0]))
+    # char.append(list(map(lambda x: x == pic.stem[0], string.ascii_letters)))
+
+inp = np.array(inp)
+
+char = np.array(char)
+
+char = keras.utils.to_categorical(char, nums)
+
+model = Sequential()
+
+# model.add(Dense(units=64, activation='relu', input_dim=100))
+model.add(Dense(units=128, activation='relu', input_shape=(256,)))
+model.add(Dense(units=64, activation='relu'))
+model.add(Dense(units=nums, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(inp, char, epochs=50, batch_size=32)
+
+model.save('mymodel')
