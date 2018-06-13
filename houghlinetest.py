3d94e59b9a1b1aaa4af3b099065412e1189c0ca8
rybin <forknispoon@gmail.com>
2018-06-13 13:46:26 +0300

    fix hough

--- houghlinetest.py
+++ houghlinetest.py
@@ -15,6 +15,7 @@ edges = cv2.Canny(thresh, 50, 150, apertureSize=3)
 # exit()
 
 lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+ttt = []
 for line in lines:
     for rho, theta in line:
         a = np.cos(theta)
@@ -26,16 +27,38 @@ for line in lines:
         x2 = int(x0 - 1000 * (-b))
         y2 = int(y0 - 1000 * (a))
 
-        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        print(rho, theta)
-        print(a, b)
+        # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        # print(rho, theta)
+        # print(a, b)
+        # ttt.append(theta)
         ttt = theta
     break
 
 # cv2.imwrite('houghlines3.jpg', img)
 
-M = cv2.getRotationMatrix2D((img.shape[0] // 2, img.shape[1] // 2), -ttt, 1)
-img = cv2.warpAffine(img, M, (img.shape[0], img.shape[1]))
+print(ttt)
+ttt = 90 - np.rad2deg(ttt)
+# exit()
+
+M = cv2.getRotationMatrix2D((img.shape[1] // 2, img.shape[0] // 2), -ttt, 1)
+img = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]), borderValue=(255, 255, 255))
+
+
+def lines(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY_INV)
+    a = np.sum(thresh, axis=1)
+    # print(a)
+    maxx = thresh.shape[1] / 100
+    last = 0
+    for j, i in enumerate(a):
+        if i < maxx:
+            if last < j - 1:
+                yield image[last:j]
+            last = j
+            image[j] = 0, 255, 0
+
+list(lines(img))
 
 cv2.imshow('img', img)
 cv2.waitKey()
